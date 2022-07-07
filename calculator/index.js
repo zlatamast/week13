9d3ae63383cec59a054f67d79bade0aaf5887a8b
zlatamast <zlatamast@gmail.com>
2022-07-07 11:21:06 +0300

    add w13 changes

---
+++ calculator/index.js
@@ -0,0 +1,20 @@
+let a = prompt(`Какое будет первое число?`, `Напиши сюда число`);
+
+let b = prompt(`Какое будет второе число?`, `Напиши сюда число`);
+
+function calcSum() {
+    alert( +a + +b );
+};
+
+function calcSubt() {
+    alert( +a - +b );
+};
+
+function calcMult() {
+    alert( +a * +b );
+};
+
+function calcDiv() {
+    alert( +a / +b );
+};
+
